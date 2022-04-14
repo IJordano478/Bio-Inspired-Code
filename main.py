ee09ef55da79f2c2662a53d253abb4cc761972b1
IJordano478 <17044826@brookes.ac.uk>
2022-04-14 16:37:08 +0100

    Added in new swarm structure. Current swarm behaviour randomly moves while other robots are in range

--- main.py
+++ main.py
@@ -31,6 +31,55 @@ y = np.arange(0, world_size_y + 1, 1)
 Z = np.ones((y.shape[0] - 1, x.shape[0] - 1)) * -1  # Intensity
 
 
+class Swarm:
+
+    def __init__(self, n_robots: int, map: np.array):
+        self.swarm_list = np.ndarray(shape=n_robots, dtype=self.Robot, order='F')
+        self.ground_truth = map
+
+        for i in range(0, n_robots):
+            start_pos = np.array([random.randint(0, map.shape[1]), random.randint(0, map.shape[0])])
+            self.swarm_list[i] = self.Robot(start_pos, map.shape)
+
+    class Robot:
+        def __init__(self, coord, map_shape):
+            self.position = coord
+            self.map = np.zeros(map_shape)
+
+    def get_closest_n(self, robot, n=1):
+        # Generate a 2d array of all robot positions
+        robot: Swarm.Robot
+        positions = np.ones((0, 2))
+        for r in self.swarm_list:
+            r: Swarm.Robot
+            positions = np.vstack((positions, r.position))
+
+        # Euclidean distance and return the objects of closest N robots
+        d = robot.position - positions
+        indexlist = np.argsort(np.linalg.norm(d, axis=1))
+        return self.swarm_list[indexlist[1:n+1]]
+
+    def get_within_d(self, robot, max_d=10):
+        # Generate a 2d array of all robot positions
+        robot: Swarm.Robot
+        positions = np.ones((0, 2))
+        for r in self.swarm_list:
+            r: Swarm.Robot
+            positions = np.vstack((positions, r.position))
+
+        # Euclidean distance and return the objects within specified d
+        d = np.linalg.norm(robot.position - positions, axis=1)
+        in_range_robots = self.swarm_list[d < max_d]
+        return in_range_robots[1:]
+
+    def share_map(self, robot1, robot2):
+        robot1: Swarm.Robot
+        robot2: Swarm.Robot
+        new_map = np.maximum(robot1.map, robot2.map)
+        robot1.map = new_map
+        robot2.map = new_map
+        return
+
 def generate_map(world_size_x, world_size_y, n_points, blur_size):
     # Generate the number of points, place on the map, and then just gaussian blur the points to make a distribution
     # Returns: map_truth,  a map of the world containing randomised nitrogen, phosphorus and potassium values
@@ -98,6 +147,24 @@ def verify_robot_position(swarm, world_size_x, world_size_y):
     return swarm
 
 
+def verify_single_robot_position(robot: Swarm.Robot, world_size_x, world_size_y):
+    # check lower
+    robot.position[robot.position < 0] = 0
+
+    # check x
+    if (robot.position[0] > world_size_x - 1):
+        robot.position[0] = world_size_x - 1
+
+    # check y
+    if (robot.position[1] > world_size_y - 1):
+        robot.position[1] = world_size_y - 1
+
+    return
+
+
+
+
+
 # fig, ax = plt.subplots(figsize=(10, 10))
 plt.ion()
 plt.show()
@@ -106,17 +173,29 @@ fig, ax = plt.subplots(2, 3, figsize=(5, 5))
 
 
 map_truth = generate_map(world_size_x, world_size_y, n_points, blur_size)
-plot_world(map_truth, swarm)
-print("started")
+swarm = Swarm(20, map_truth)
+plot_world(map_truth)
 time.sleep(3)
-print("finished")
 
 # for i in range(0, 10):
 while (True):
-    random_move = np.random.randint(-2, 3, size=(n_robots, 2))
-    swarm = swarm + random_move
-    swarm = verify_robot_position(swarm, world_size_x, world_size_y)
+    for r in swarm.swarm_list:
+        if swarm.get_within_d(r).shape[0] > 0:
+            random_move = np.random.randint(-2, 3, size=2)
+            r.position += random_move
+            verify_single_robot_position(r, world_size_x, world_size_y)
+        else:
+            pass
 
-    plot_world(map_truth, swarm)
-    print("looped")
+    positions = np.ones((0, 2))
+    for r in swarm.swarm_list:
+        r: Swarm.Robot
+        positions = np.vstack((positions, r.position))
+    plot_world(map_truth, positions)
+    # random_move = np.random.randint(-2, 3, size=(n_robots, 2))
+    # swarm = swarm + random_move
+    # swarm = verify_robot_position(swarm, world_size_x, world_size_y)
+    #
+    # plot_world(map_truth, swarm)
+    # print("looped")
     time.sleep(0.5)
