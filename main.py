ba512f66e8d8405e5a2d8d381ab0e7b477789e2a
hrutland <hcrutland@mail.com>
2022-04-17 13:41:55 +0100

    mp4 output prototype

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import random
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-#from IPython.display import clear_output
+from celluloid import Camera
 from scipy.ndimage import gaussian_filter
+from IPython.display import HTML
 #% matplotlibinline
 
 # Seed the sim
 random.seed(1)
 np.random.seed(1)
+
+
 
 # Program Params
 n_points = random.randint(0, 100)
@@ -19,6 +22,7 @@ world_size_y = 100
 blur_size = 5
 n_robots = 20
 swarm = np.zeros((n_robots, 2))
+
 
 # Uncomment to start top right
 # swarm[:, 0] = np.random.randint(world_size_x-1, world_size_x, size=(n_robots))
@@ -51,6 +55,10 @@ def generate_map(world_size_x, world_size_y, n_points, blur_size):
     return map_truth
 
 
+fig, ax = plt.subplots(2, 3, figsize=(20, 20))
+camera = Camera(fig)
+
+
 def plot_world(map_truth, robots=None):
     # Matplot a map of the field, showing the 3 levels for nitrogen, phosphorus and potassium
     # as well as the positions of the robots and what they've detected for each element
@@ -60,7 +68,7 @@ def plot_world(map_truth, robots=None):
 
     titles = ['Ground truth nitrogen', 'Ground truth phosphorus', 'Ground truth potassium']
     # Plot map
-    fig, ax = plt.subplots(2, 3, figsize=(20, 20))
+    
     for i in range(0, 3):
         ax[0, i].pcolormesh(x, y, map_truth[:, :, i])
         ax[0, i].title.set_text(titles[i])
@@ -70,14 +78,16 @@ def plot_world(map_truth, robots=None):
     # If a 2D numpy array was given for the robots, plot them
     if robots is not None:
         for i in range(0, 3):
-            ax[0, i].scatter(robots[:, 0] + 0.5, robots[:, 1] + 0.5, c='w')
-            ax[1, i].scatter(robots[:, 0] + 0.5, robots[:, 1] + 0.5, c='k')
+            ax[0, i].scatter(robots[:, 0] + 1, robots[:, 1] + 1, c='w')
+            ax[1, i].scatter(robots[:, 0] + 1, robots[:, 1] + 1, c='k')
             ax[1, i].set_xlim([0, map_truth.shape[1]])
             ax[1, i].set_ylim([0, map_truth.shape[0]])
             ax[1, i].title.set_text(titles2[i])
             plt.gca().set_aspect('equal', adjustable='box')
+  
+        
+    camera.snap()
 
-    plt.show()
 
 
 def verify_robot_position(swarm, world_size_x, world_size_y):
@@ -93,15 +103,32 @@ def verify_robot_position(swarm, world_size_x, world_size_y):
 
 
 # fig, ax = plt.subplots(figsize=(10, 10))
+
+snap = False
 map_truth = generate_map(world_size_x, world_size_y, n_points, blur_size)
 plot_world(map_truth, swarm)
 time.sleep(3)
 
-# for i in range(0, 10):
-while (True):
+
+
+
+# fig = plt.figure()
+for i in range(0, 800):
+# while (True):
     random_move = np.random.randint(-2, 3, size=(n_robots, 2))
     swarm = swarm + random_move
     swarm = verify_robot_position(swarm, world_size_x, world_size_y)
+    if (i% 20) == 0:
+        plot_world(map_truth, swarm)
+        time.sleep(0.01)
+    print(i)
+    
+print('done')
+# animation = camera.animate()
 
-    plot_world(map_truth, swarm)
-    time.sleep(0.01)
+# animation.save('animation.gif', writer='imagemagick', fps=30)
+
+animation = camera.animate(blit=False, interval = 200, repeat = False)
+animation.save('xy.mp4')
+
+print('done')
